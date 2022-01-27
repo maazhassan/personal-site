5f916e2391a7fcb387e9630a51cdea2ecf3940c5
Maaz Hassan <h.maaz809@hotmail.com>
2022-01-27 00:21:07 -0700

    Project structure cleanup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="h-screen bg-dark-blue">
+      <p className="text-5xl text-center text-neutral-100">
+        MAAZ HASSAN
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#0d1017',
+        'text-blue': '#333e57',
+        'blue-gray': '#6f8695',
+        'blue': '#2fcccc',
+        'green': '#7ad94e',
+        'orange': '#e56f20',
+      },
+      fontFamily: {
+        'sans': ['Rubik', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
