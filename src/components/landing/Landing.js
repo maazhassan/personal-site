89d629afa9a63c73ab311c870caa81820aed2aad
Maaz Hassan <h.maaz809@hotmail.com>
2022-03-04 23:03:54 -0700

    Hint timeout clear on click

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -3,7 +3,7 @@ import SwitchBorder from './SwitchBorder';
 import Typewriter from 'typewriter-effect';
 import { animated } from 'react-spring';
 import useAnimatedFade from '../../hooks/animatedFade';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Hint from '../main/switchbar/Hint';
 
 const FADE_DELAY = 500;
@@ -12,10 +12,12 @@ const HINT_DELAY = !!(localStorage.getItem('landingSwitch')) ? 7000 : 6000;
 const Landing = props => {
   const [hintToggle, setHintToggle] = useState(false);
 
-  useEffect(() => {
-    const hintTimeout = setTimeout(() => setHintToggle(true), HINT_DELAY);
+  const hintTimeout = useRef(null);
 
-    return () => clearTimeout(hintTimeout);
+  useEffect(() => {
+    hintTimeout.current = setTimeout(() => setHintToggle(true), HINT_DELAY);
+
+    return () => clearTimeout(hintTimeout.current);
   }, []);
 
   const animProps = useAnimatedFade(props.toggle, FADE_DELAY);
@@ -28,7 +30,7 @@ const Landing = props => {
       <div className="mt-32 2xl:mt-52 3xl:mt-80 relative">
         <Switch 
           className={`h-36 2xl:h-44 3xl:h-52 absolute z-10 mx-auto left-0 right-0 my-auto top-0 bottom-0 ${props.switchStatus ? 'cursor-pointer' : ''}`}
-          onClick={() => props.onSwitchClicked()}
+          onClick={() => {props.onSwitchClicked(); clearTimeout(hintTimeout.current)}}
           isOn={props.switchStatus}
           styles={`fill-transparent ${props.switchStatus ? 'stroke-dark-blue' : 'stroke-neutral-50'} stroke-[5px]`}
           accentFill={props.switchStatus ? '#b4b4b4' : '#39454d'}
