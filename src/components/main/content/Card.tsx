89f7a1f6a553b0ab36a6a2369c87494ec24c57bb
Maaz Hassan <h.maaz809@hotmail.com>
2026-03-15 03:33:56 -0600

    Project card fixes.
    - fix alt text to be project title
    - remove imageRendering webkit thing

--- src/components/main/content/Card.tsx
+++ src/components/main/content/Card.tsx
@@ -29,9 +29,8 @@ const Card = ({ image, title, stack, github, onHover, index }: CardProps) => {
         <div className="flex flex-col absolute w-full h-full items-center top-0">
           <img
             src={image}
-            alt="Site logo"
+            alt={title}
             className="w-24 sm:w-32 md:w-36 lg:mt-8"
-            style={{ imageRendering: '-webkit-optimize-contrast' as const }}
           />
           <span className="text-xl sm:text-2xl 3xl:text-3xl text-neutral-50 mt-3 md:mt-7">{title}</span>
           <span className="text-base sm:text-lg 3xl:text-2xl text-text-blue">{stack}</span>
