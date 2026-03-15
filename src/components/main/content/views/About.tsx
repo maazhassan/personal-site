bd2049635069cba22e3d6004d058d74ece4a69b6
Maaz Hassan <h.maaz809@hotmail.com>
2026-03-15 03:04:44 -0600

    Add job

--- src/components/main/content/views/About.tsx
+++ src/components/main/content/views/About.tsx
@@ -17,14 +17,15 @@ const About = ({ toggle }: AboutProps) => {
 
   const divAnimProps = useAnimatedFade(toggle && mountToggle);
   const uniULAnimProps = useAnimatedUnderline(hover === 0);
-  const synULAnimProps = useAnimatedUnderline(hover === 1);
+  const resULAnimProps = useAnimatedUnderline(hover === 1);
+  const synULAnimProps = useAnimatedUnderline(hover === 2);
 
   return (
     <animated.div
       {...divAnimProps}
       className="absolute w-full h-full top-0 text-center flex flex-col justify-center items-center px-10 pb-6"
     >
-      <p className="text-2xl md:text-4xl 3xl:text-5xl text-neutral-50 w-full md:w-[80%] lg:w-1/2 xl:w-[40%] 2xl:w-1/3">
+      <p className="text-xl md:text-2xl 3xl:text-3xl text-neutral-50 w-full md:w-[80%] lg:w-1/2 xl:w-[40%] 2xl:w-1/4">
         <span>A software developer graduate from the </span>
         <a
           href="https://www.ucalgary.ca/"
@@ -42,13 +43,30 @@ const About = ({ toggle }: AboutProps) => {
         </a>.
         <br /><br />
 
-        <span>Former intern at </span>
+        <span>Currently building the next generation of well information management software at </span>
+        <a
+          href="https://resourceenergysolutions.com/"
+          className="text-red-600 hover:cursor-pointer relative"
+          target="_blank"
+          rel="noreferrer"
+          onMouseOver={() => { if (window.innerWidth > 768) setHover(1); }}
+          onMouseOut={() => setHover(null)}
+        >
+          Resource Energy Solutions
+          <animated.span
+            {...resULAnimProps}
+            className="bg-current absolute h-[2px] w-full my-0 bottom-0 left-0"
+          />
+        </a>.
+        <br /><br />
+
+        {/* <span>Former intern at </span>
         <a
           href="https://www.synopsys.com/"
           className="text-[#5a2a82] hover:cursor-pointer relative"
           target="_blank"
           rel="noreferrer"
-          onMouseOver={() => { if (window.innerWidth > 768) setHover(1); }}
+          onMouseOver={() => { if (window.innerWidth > 768) setHover(2); }}
           onMouseOut={() => setHover(null)}
         >
           Synopsys
@@ -57,7 +75,7 @@ const About = ({ toggle }: AboutProps) => {
             className="bg-current absolute h-[2px] w-full my-0 bottom-0 left-0"
           />
         </a>.
-        <br /><br />
+        <br /><br /> */}
 
         <span>Click a switch below to see some of my work!</span>
       </p>
